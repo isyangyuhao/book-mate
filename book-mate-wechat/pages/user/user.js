0c3401b0aa42c57a8521932fdf6f935b40bc7273
yang <isyangyuhao@gmail.com>
2017-04-24 10:15:08 +0800

    完成基础页面
    login, register, index, main, books, forum, user

--- book-mate-wechat/pages/user/user.js
+++ book-mate-wechat/pages/user/user.js
@@ -1,19 +1,22 @@
 // pages/user/user.js
 Page({
-  data:{},
-  onLoad:function(options){
-    // 页面初始化 options为页面跳转所带来的参数
+  data: {
+    userInfo: {}
   },
-  onReady:function(){
-    // 页面渲染完成
+  onLoad: function (options) {
+    var app = getApp();
+    app.getUserInfo();
+    var that = this;
+    wx.getUserInfo({
+      success: function (res) {
+        var userInfo = res.userInfo;
+        that.setData({ userInfo: userInfo });
+      }
+    })
   },
-  onShow:function(){
-    // 页面显示
-  },
-  onHide:function(){
-    // 页面隐藏
-  },
-  onUnload:function(){
-    // 页面关闭
+  exitBtnOnClick: function() {
+    wx.redirectTo({
+      url: '/pages/index/index'
+    })
   }
 })
